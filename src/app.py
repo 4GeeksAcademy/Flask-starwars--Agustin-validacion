b64a36ac0289577420ff59ea2ea9168d7f470af6
Agustin Rolando <138535452+Agustin144@users.noreply.github.com>
2023-08-31 20:48:35 +0000

    medio protegido

--- src/app.py
+++ src/app.py
@@ -670,12 +670,9 @@ def login():
 @jwt_required()
 def protected():
     # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
-
-
-if __name__ == "__main__":
-    app.run()
+    current_user_id = get_jwt_identity()
+    user=User.filter.get(current_user_id)
+    return jsonify({"id": user.id, "username":user.username}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
